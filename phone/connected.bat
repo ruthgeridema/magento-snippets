213767805037d4c1fc36c85a7df7ff55881d3283
ruthgeridema <ruthgeridema@gmail.com>
2017-01-20 15:05:56 +0100

    Update connected.bat
    dont open if anonymous

--- phone/connected.bat
+++ phone/connected.bat
@@ -1,6 +1,7 @@
 @echo OFF
 
 IF "%~1" == "" GOTO End
+IF "%~1" == "Anonymous" GOTO End
 start "" "https://www.yoururl.com/directory/file.php?phonenumber=%1&apikey=9rxP28m90F64oLAJ7dM4Qxy05q9bQXWm"
 
 :End
