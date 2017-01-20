90913e7346fec1a448d33de908f026b2d6bc6377
ruthgeridema <ruthgeridema@gmail.com>
2017-01-20 13:24:23 +0100

    Update connected.bat

--- phone/connected.bat
+++ phone/connected.bat
@@ -1,3 +1,3 @@
 @echo OFF
 
-start "" "https://www.yoururl.com/directory/file.php?phonenumber=%1"
+start "" "https://www.yoururl.com/directory/file.php?phonenumber=%1&apikey=9rxP28m90F64oLAJ7dM4Qxy05q9bQXWm"
