a13324c58af836f857a2920f753fb2ccb63fa827
ruthgeridema <ruthgeridema@gmail.com>
2017-01-20 11:28:04 +0100

    Update open-order-on-call.php

--- phone/open-order-on-call.php
+++ phone/open-order-on-call.php
@@ -1,8 +1,13 @@
 <?php
-$phonenumber = htmlspecialchars($_POST["postalcode"]);
 
-date_default_timezone_set('Europe/Amsterdam');
-$tijd = date('H:i:s', time());
+//SETTINGS
+$phonenumber    = htmlspecialchars($_POST["postalcode"]);
+$leavecomment   = 1; //1 for yes, 0 for no
+$comment        = "Customer called!";
+$adminurl       = "https://magentoadmin.com/admin";
+$time           = date('H:i:s', time());
+
+
 require_once '../app/Mage.php';
 Mage::app();
 umask(0);
@@ -10,43 +15,23 @@ umask(0);
 
 $CustomAddress = Mage::getModel('sales/order_address')->getCollection()->addAttributeToFilter('telephone', $phonenumber)->getLastItem();
 
-
 $orders = Mage::getModel('sales/order')->getCollection()
     ->addFieldToFilter('shipping_address_id', $CustomAddress->getId())
     ->addAttributeToSelect('*')
     ;
 
-
 foreach ($orders as $order) {
-
-if($_GET["opnemer"] == '203'){
-$_GET["opnemer"] = "Ruthger";
-} else if($_GET["opnemer"] == '207'){
-$_GET["opnemer"] = "Rita";
-} else if($_GET["opnemer"] == '202'){
-$_GET["opnemer"] = "Kantoor 2";
-} else if($_GET["opnemer"] == '201'){
-$_GET["opnemer"] = "Kantoor 1";
-} 
-
-
-
-
-$laatste = $order->getId();
-$history = $order->addStatusHistoryComment('Klant belde, opgenomen door ' . $_GET["opnemer"] , false);
-$history->setIsCustomerNotified(false);
-
-$order->save();
-
-break;
+    $laatste = $order->getId();
+    $history = $order->addStatusHistoryComment($comment , false);
+    $history->setIsCustomerNotified(false);
+    $order->save();
+    break;
 }
 
 
 if($laatste){
-header("Location: http://www.witjesverzendhuis.com/index.php/beheer/sales_order/view/order_id/". $laatste . "/");
-die();
+    header("Location: " . $adminurl . "/sales_order/view/order_id/". $laatste . "/");
+    die();
 } else {
-
     echo "<script>window.close();</script>";
-
 }
