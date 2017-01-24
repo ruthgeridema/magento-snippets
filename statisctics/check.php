656afb6f45765280e3431f75ef7c6a0492fe9a3a
ruthgeridema <ruthgeridema@gmail.com>
2017-01-24 16:44:44 +0100

    Create check.php

---
+++ statisctics/check.php
@@ -0,0 +1,18 @@
+<?php
+//Gets the total of processing orders
+//Will be displayed by index.html via AJAX
+
+require_once '../app/Mage.php';
+Mage::app();
+umask(0);
+
+$fromDate = date('Y-m-d H:i:s', strtotime("-1 week")); //in our case one week, because otherwise backorders are counted aswell
+$toDate = date('Y-m-d H:i:s', strtotime("now"));
+
+
+$processingOrders = Mage::getModel('sales/order')->getCollection()
+        ->addFieldToFilter('state', 'processing') 
+        ->addAttributeToFilter('created_at', array('from'=>$fromDate, 'to'=>$toDate))
+        ->addAttributeToSelect('created_at');
+        
+echo $processingOrders->count();
